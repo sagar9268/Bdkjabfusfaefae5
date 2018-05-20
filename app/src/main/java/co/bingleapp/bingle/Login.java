8e2543bae8c99e33f3aa8430173705d111eded16
Lukethered <spsmail13@gmail.com>
2018-05-21 01:36:23 +0530

    2nd commit

--- app/src/main/java/co/bingleapp/bingle/Login.java
+++ app/src/main/java/co/bingleapp/bingle/Login.java
@@ -3,11 +3,11 @@ package co.bingleapp.bingle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Home_Page extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home__page);
+        setContentView(R.layout.activity_login);
     }
 }
