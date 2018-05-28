e2738e0e964f29c8ab3fa1375d3d8bec7e7bb405
Lukethered <spsmail13@gmail.com>
2018-05-28 21:04:39 +0530

    Linked Login and SignUp screens

--- app/src/main/java/co/bingleapp/bingle/Login.java
+++ app/src/main/java/co/bingleapp/bingle/Login.java
@@ -1,7 +1,11 @@
 package co.bingleapp.bingle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Linked elements in layout to Java code
+        TextView mSignupSwitch =  findViewById(R.id.SignUp_TxtViewButton);
+
+        //Switch to Sign up activity
+        mSignupSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mSwitchtoSignUp = new Intent(Login.this,SignUp.class);
+                startActivity(mSwitchtoSignUp);
+            }
+        });
+
     }
 }
--- app/src/main/java/co/bingleapp/bingle/SignUp.java
+++ app/src/main/java/co/bingleapp/bingle/SignUp.java
@@ -1,7 +1,10 @@
 package co.bingleapp.bingle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        // Linked elements in layout to Java code
+        TextView mLoginSwitch = findViewById(R.id.Login_TextViewButton);
+
+        // Switch to Login activity
+        mLoginSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mSwitchtoLogin = new Intent(SignUp.this,Login.class);
+                startActivity(mSwitchtoLogin);
+            }
+        });
     }
 }
