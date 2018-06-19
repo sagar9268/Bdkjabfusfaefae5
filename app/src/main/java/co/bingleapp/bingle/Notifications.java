3aa8a838ad8f3bf4c94721469b5edfc23ded92e3
Sagar <sagar9268@yahoo.com>
2018-06-19 05:53:32 +0530

    notifications and settings activity created bottom navigation added gradle file also updated navigation menu created

---
+++ app/src/main/java/co/bingleapp/bingle/Notifications.java
@@ -0,0 +1,15 @@
+package co.bingleapp.bingle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Notifications extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications);
+
+    }
+}
---
+++ app/src/main/java/co/bingleapp/bingle/Settings.java
@@ -0,0 +1,15 @@
+package co.bingleapp.bingle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+    }
+}
