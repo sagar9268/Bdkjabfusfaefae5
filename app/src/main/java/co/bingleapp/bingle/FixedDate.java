b9c729f273f9c1866130dcf82384c34e258b084c
Sagar <sagar9268@yahoo.com>
2018-06-28 04:18:25 +0530

    layout changed bottom navigation new menu added and updated new activity added fixed date settings activity changed main activity changed manifests file updates new strings added

---
+++ app/src/main/java/co/bingleapp/bingle/FixedDate.java
@@ -0,0 +1,14 @@
+package co.bingleapp.bingle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class FixedDate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fixed_date);
+    }
+}
